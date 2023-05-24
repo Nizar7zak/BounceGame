6f03834493e804c5d9f873758c701aec359cf8e6
nizar7zak <nizar7zakout@gmail.com>
2023-05-24 16:56:05 +0200

    handle position animation of limbo block

--- src/Level.jsx
+++ src/Level.jsx
@@ -12,12 +12,27 @@ const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
 const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
 const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
 
+const BlockStart = ({ position=[ 0, 0, 0 ] }) => {
+    return (
+      <group position={position}>
+  
+        <mesh
+            geometry={boxGeometry}
+            material={floor1Material}
+            scale={[ 4, 0.2, 4 ]} 
+            position={[ 0, -0.1, 0 ]}
+            receiveShadow
+        />
+  
+      </group>
+    )
+}
+
 
 const BlockSpinner = ({ position=[ 0, 0, 0 ] }) => {
     const obstacle = useRef()
 
     const [ speed ] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1) )
-    console.log(speed)
     useFrame((state) => {
         const time = state.clock.elapsedTime
 
@@ -55,29 +70,51 @@ const BlockSpinner = ({ position=[ 0, 0, 0 ] }) => {
     )
 }
 
+const BlockLimbo = ({ position=[ 0, 0, 0 ] }) => {
+    const obstacle = useRef()
 
-const BlockStart = ({ position=[ 0, 0, 0 ] }) => {
+    const [ timeOffset ] = useState(() => (Math.random() * Math.PI * 2 ) )
+    useFrame((state) => {
+        const time = state.clock.elapsedTime
+
+        const y = Math.sin(time + timeOffset) + 1.15
+        obstacle.current.setNextKinematicTranslation({x: position[0], y: position[1] + y, z: position[2]})
+    })
+
     return (
       <group position={position}>
   
         <mesh
             geometry={boxGeometry}
-            material={floor1Material}
+            material={floor2Material}
             scale={[ 4, 0.2, 4 ]} 
             position={[ 0, -0.1, 0 ]}
             receiveShadow
         />
+
+        <RigidBody ref={obstacle} type="kinematicPosition" position={[ 0, 0.3, 0]} restitution={0.2} friction={0} >
+
+            <mesh
+                geometry={boxGeometry}
+                material={obstacleMaterial}
+                scale={[ 3.5, 0.3, 0.3 ]} 
+                castShadow
+                receiveShadow
+            />
+
+        </RigidBody>
   
       </group>
     )
 }
 
-  
 const Level = () => {
   return (
     <>
-        <BlockStart position={[ 0, 0, 4 ]} />
-        <BlockSpinner position={[ 0, 0, 0 ]} />
+        <BlockStart position={[ 0, 0, 8 ]} />
+        <BlockSpinner position={[ 0, 0, 4 ]} />
+        <BlockLimbo position={[ 0, 0, 0 ]} />
+        <BlockLimbo position={[ 0, 0, -4 ]} />
     </>
   )
 }
