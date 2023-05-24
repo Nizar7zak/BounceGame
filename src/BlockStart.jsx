55c3c0680af4e48e22dea71c7fc2b3cb03ee1477
nizar7zak <nizar7zakout@gmail.com>
2023-05-24 16:04:11 +0200

    create geometry and material by using THREE.js

--- src/BlockStart.jsx
+++ src/BlockStart.jsx
@@ -1,10 +1,26 @@
+import * as THREE from 'three'
+
+THREE.ColorManagement.legacyMode = false
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+ 
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
+const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
+const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+
 const BlockStart = ({ position=[ 0, 0, 0 ] }) => {
   return (
     <group position={position}>
-        <mesh position={[ 0, -0.1, 0 ]} receiveShadow >
-            <boxGeometry args={[ 4, 0.2, 4 ]} />
-            <meshStandardMaterial color='limegreen' />
-        </mesh>
+
+        <mesh
+            geometry={boxGeometry}
+            material={floor1Material}
+            scale={[ 4, 0.2, 4 ]} 
+            position={[ 0, -0.1, 0 ]}
+            receiveShadow
+        />
+
     </group>
   )
 }
