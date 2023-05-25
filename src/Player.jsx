5ad58d9df7b4fa7db2ea0987794e4d755dc21721
nizar7zak <nizar7zakout@gmail.com>
2023-05-25 10:55:34 +0200

    add colliders ball, restitution and friction to physics

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,14 +1,12 @@
 import { RigidBody } from "@react-three/rapier"
 
 const Player = () => {
-  return (
-    <RigidBody>
+  return <RigidBody position={[ 0, 1, 0 ]} colliders='ball' restitution={0.2} friction={1} >
         <mesh castShadow >
             <icosahedronGeometry args={[ 0.3, 1 ]} />
             <meshStandardMaterial flatShading color='mediumpurple' />
         </mesh>
     </RigidBody>
-  )
 }
 
 export default Player
