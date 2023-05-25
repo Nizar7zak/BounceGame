b254d89154c6d3c1cc6fac4c35017cfc8d9bf77d
nizar7zak <nizar7zakout@gmail.com>
2023-05-25 12:53:20 +0200

    handle raycast from rapier

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,19 +1,33 @@
 import { useFrame } from "@react-three/fiber"
-import { RigidBody } from "@react-three/rapier"
+import { RigidBody, useRapier } from "@react-three/rapier"
 import { useKeyboardControls } from "@react-three/drei"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
 const Player = () => {
     const body = useRef()
+
     const [ subscribeKeys, getKeys ] = useKeyboardControls()
 
+    const { rapier, world } = useRapier()
+    const rapierWorld = world.raw()
+
     const jump = () => {
-        console.log('Jump?')
+        const origin = body.current.translation()
+        origin.y -= 0.31
+        const direction = { x: 0, y: -1, z: 0 }
+
+        const ray = new rapier.Ray(origin, direction) 
+        const hit = rapierWorld.castRay(ray, 10, true)
+
+        if(hit.toi < 0.15) {
+            body.current.applyImpulse({ x: 0, y: 0.5 , z: 0 })
+        }
+
     }
 
-    useFrame(() => {
+    useEffect(() => {
         subscribeKeys(
-            state => state.jump,
+            (state) => state.jump,
             (value) => {
                 if(value){
                     jump()
