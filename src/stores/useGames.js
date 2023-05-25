08b08c6e3c48a367c4d5d23d507e1dab050c3f83
nizar7zak <nizar7zakout@gmail.com>
2023-05-25 14:53:55 +0200

    methods of phases inside store

--- src/stores/useGames.js
+++ src/stores/useGames.js
@@ -1,7 +1,25 @@
 import  { create } from "zustand";
 
-export default create(() => {
+export default create((set) => {
     return {
-        blockCount: 7
+        blockCount: 7,
+        phase: 'ready',
+        start: () => {
+            set(() => {
+                return { phase: 'playing' }
+            })
+        },
+        restart: () => {
+            set(() => {
+                return { phase: 'ready' }
+            })
+        },
+        end: () => {
+            set(() => {
+                return { phase: 'ended' }
+            })
+        },
+
+
     }
 })
