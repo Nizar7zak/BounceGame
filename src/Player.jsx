eac75fc73d57c1447bbf514d15f0e7650d210718
nizar7zak <nizar7zakout@gmail.com>
2023-05-25 11:28:13 +0200

    handle movement|

--- src/Player.jsx
+++ src/Player.jsx
@@ -14,6 +14,26 @@ const Player = () => {
         const impulse = { x: 0, y: 0, z: 0 }
         const torque = { x: 0, y: 0, z: 0 }
 
+        const impulseStrength = 0.6 * delta
+        const torqueStrength = 0.2 * delta
+
+        if(forward) {
+            impulse.z -= impulseStrength
+            torque.x -= torqueStrength
+        }
+        if(backward) {
+            impulse.z += impulseStrength
+            torque.x += torqueStrength
+        } 
+        if(leftward) {
+            impulse.x -= impulseStrength
+            torque.z += torqueStrength
+        }
+        if(rightward) {
+            impulse.x += impulseStrength
+            torque.z -= torqueStrength
+        }
+
         body.current.applyImpulse(impulse)
         body.current.applyTorqueImpulse(torque)
     })
