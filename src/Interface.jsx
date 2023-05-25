9e23fcc07148ed47b6c74f31144e9588157c8a00
nizar7zak <nizar7zakout@gmail.com>
2023-05-25 14:11:29 +0200

    add keys on the screen

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -5,6 +5,24 @@ const Interface = () => {
         <div className="time">0.00</div>
         <div className="restart">Restart</div>
 
+        <div className="controls">
+
+            <div className="raw">
+                <div className="key"></div>
+            </div>
+
+            <div className="raw">
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+            </div>
+
+            <div className="raw">
+                <div className="key large"></div>
+            </div>
+
+        </div>
+
     </div>
   )
 }
